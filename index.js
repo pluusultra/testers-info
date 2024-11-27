23c605888b68dd6daaea5088f45a5c60678c11f2
pluusultra <ultracrackerrr@gmail.com>
2024-11-27 19:33:30 +0700

    реализовал копирование в буфер обмена после клика на элемент

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {itemsArray} from './items.js'
+
 const inputSearch = document.querySelector('.test')
 const noResult = document.querySelector('.nothing')
 const template = document.querySelector('#template')
@@ -7,6 +8,21 @@ const itemsList = document.querySelector('tbody')
 // Добавляем на сайт массив с информацией о каждом предмете
 itemsArray.forEach((el) => {
     const itemElement = template.content.cloneNode(true)
+    const itemContainer = itemElement.querySelector('.table-item')
+    itemContainer.addEventListener('click', (e) => {
+        let originalText = e.target.textContent;
+        if (originalText !== 'Скопировано') {
+            navigator.clipboard.writeText(e.target.textContent);
+            e.target.classList.add('copied')
+            e.target.textContent = 'Скопировано';
+            setTimeout(() => {
+                e.target.textContent = originalText;
+                e.target.classList.remove('copied')
+            }, 1000)
+        } else {
+            console.log('скопировано')
+        }
+    })
     let itemId = itemElement.querySelector('.item-id')
     let modelId = itemElement.querySelector('.model-id')
     let itemName = itemElement.querySelector('.item-name')
